e99e79b2529e3af230e03ddd0c9ff7d7d5802768
Thiago Ozores <tozores@gmail.com>
2021-03-15 17:00:03 -0300

    atualizando output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "gateway" {
-  value = "http://${module.azure-webserver-cluster.public_ip_address}"
+  value = "http://${module.azure-web-app-ha.public_ip_address}"
 }
 
 output "bastion" {
-  value = "ssh ${var.username}@${module.azure-webserver-cluster.bastion_public_ip_address}"
+  value = "ssh ${var.username}@${module.azure-web-app-ha.bastion_public_ip_address}"
 }
 
