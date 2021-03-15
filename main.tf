79e448771d94a78d59fe346cdd34342e3b8c58fd
Thiago Ozores <tozores@gmail.com>
2021-03-15 16:54:26 -0300

    atualizando source do modulo

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "azure-web-app-ha" {
-  source = "git@github.com:ozorest/azure-web-server-cluster.git"
+  source = "git@github.com:ozorest/azure-web-app-ha.git"
 
   project = "xpto"
   location = "brazilsouth"
